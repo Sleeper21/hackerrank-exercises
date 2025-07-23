5ce16e4e5589db76871062587880c565ff06359b
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2025-07-23 11:38:48 +0100

    eliminate boiler plate code from hacker rank

--- Go/missing-numbers/missing_numbers.go
+++ Go/missing-numbers/missing_numbers.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
 )
 
 /*
@@ -34,74 +29,11 @@ func missingNumbers(arr []int32, brr []int32) []int32 {
 
 	}
 
+	//  TODO: Continue
+
+	return []int32{}
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
-
-	defer stdout.Close()
-
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
-
-	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-
-	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-
-	var arr []int32
-
-	for i := 0; i < int(n); i++ {
-		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
-		checkError(err)
-		arrItem := int32(arrItemTemp)
-		arr = append(arr, arrItem)
-	}
-
-	mTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-	m := int32(mTemp)
-
-	brrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-
-	var brr []int32
-
-	for i := 0; i < int(m); i++ {
-		brrItemTemp, err := strconv.ParseInt(brrTemp[i], 10, 64)
-		checkError(err)
-		brrItem := int32(brrItemTemp)
-		brr = append(brr, brrItem)
-	}
-
-	result := missingNumbers(arr, brr)
-
-	for i, resultItem := range result {
-		fmt.Fprintf(writer, "%d", resultItem)
-
-		if i != len(result)-1 {
-			fmt.Fprintf(writer, " ")
-		}
-	}
-
-	fmt.Fprintf(writer, "\n")
-
-	writer.Flush()
-}
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Hello, missing numbers!")
 }
